c09a8c7a57c2403c4df6b169844687eb702e2291
21croissants <jean-michel@21croissants.com>
2011-08-15 15:41:59 +0200

    WIP I should have committed a long time ago

--- plugins/rails/rails.plugin.zsh
+++ plugins/rails/rails.plugin.zsh
@@ -4,9 +4,12 @@ alias sd='ruby script/destroy'
 alias sp='ruby script/plugin'
 alias ssp='ruby script/spec'
 alias rdbm='rake db:migrate'
+alias rdbt='rake RAILS_ENV=test db:migrate'
+alias rdbtr='rake RAILS_ENV=test db:migrate:redo'
 alias sc='ruby script/console'
 alias sd='ruby script/server --debugger'
 alias devlog='tail -f log/development.log'
+alias dep_staging='git svn dcommit;cap deploy'
 
 function remote_console() {
   /usr/bin/env ssh $1 "( cd $2 && ruby script/console production )"
